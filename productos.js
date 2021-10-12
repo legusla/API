d242756840c9841c566fd5ed2a906b8f217529a0
legusla <leonardoscarcella@yahoo.com>
2021-10-12 17:39:09 -0300

    a

---
+++ productos.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const productosRouter = express.Router();
+
+productosRouter.post('/productos', async(req, res) => {
+    const nuevoProducto = req.body;
+
+    const productoGuardado = await productosContenedor.save(nuevoProducto);
+    res.render('pages/list-productos');
+    res.send(...nuevoProducto);
+    console.log(productoGuardado);
+});
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/index')
+});
+
+app.get('/form', function(req, res) {
+    res.render('pages/form');
+});
+
+app.get('/productos', function (req,res){
+    res.render('pages/list-productos')
+});
+
+app.post('/productos', function (req,res){
+    res.render('pages/list-productos')
+});
+
+app.use(function (err, req, res, next) {
+    console.error(error.stack);
+    res.status(500).send('Algo se rompio!');
+})
+
+app.listen(8080);
+app.on('error', (error) => console.log('Error', error));
+console.log('el puerto 8080 se esta ejecutando');
