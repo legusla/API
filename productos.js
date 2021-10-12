289049b23ff4afba8b49f8717d35f44c2b05ccf6
legusla <leonardoscarcella@yahoo.com>
2021-10-12 19:03:38 -0300

    a

--- productos.js
+++ productos.js
@@ -2,16 +2,23 @@ const express = require('express');
 
 const Contenedor = require('./Contenedor');
 
-const productosContenedor = new Contenedor('./productos.json');
+const productosContenedor = new Contenedor('./productos.js');
 
 const productosRouter = express.Router();
+
+productosRouter.get('/productos', async(req, res) => {
+    const lista = await productosContenedor.getAll();
+    res.send({
+        data: lista
+    });
+    res.render('pages/list-productos');
+})
 
 productosRouter.post('/productos', async(req, res) => {
     const nuevoProducto = req.body;
 
     const productoGuardado = await productosContenedor.save(nuevoProducto);
     res.render('pages/list-productos');
-    res.send(...nuevoProducto);
     console.log(nuevoProducto);
 });
 
